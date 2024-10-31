85d7a0f44ea26a5d80b3e6d3dc9b4c16c9bba580
horspaz064 <hormc1234@gmial.com>
2024-10-31 15:02:55 +0700

    new project create

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# ProjectProduct
+# TeamProject
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.5.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.10.
 
 ## Development server
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Project-Product' title`, () => {
+  it(`should have the 'Team-Project' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Project-Product');
+    expect(app.title).toEqual('Team-Project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Project-Product');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Team-Project');
   });
 });
